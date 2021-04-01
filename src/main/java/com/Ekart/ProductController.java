45491fd1b49bf6b6a1fdff1dd5b98975b1c0bee2
vinay <vvinaybhargav@gmail.com>
2021-04-01 18:41:48 +0530

    deleted

--- src/main/java/com/Ekart/ProductController.java
+++ src/main/java/com/Ekart/ProductController.java
@@ -5,11 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -63,7 +63,7 @@ public class ProductController {
 		return repo.findAll();
 	}
 
-	@DeleteMapping("deleteProduct/{pid}")
+	@RequestMapping("deleteProduct/{pid}")
 	public List<Product> deleteProduct(@PathVariable("pid") String id) {
 		System.out.println(id);
 		repo.deleteByPid(id);
